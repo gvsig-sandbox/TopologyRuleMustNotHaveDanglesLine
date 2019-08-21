974141286cab331ebe5ddab56e7e71cc51b2ef67
hecnita <hectorth23@gmail.com>
2019-08-21 11:56:18 +0200

    add tolerance

--- mustNotHaveDanglesLineRule.py
+++ mustNotHaveDanglesLineRule.py
@@ -49,58 +49,51 @@ class MustNotHaveDanglesLineRule(AbstractTopologyRule):
     try:
       print "Starts with: ", feature1.Name
       logger("si", LOGGER_INFO)
-      #store2 = self.getDataSet2().getFeatureStore()
-      #tolerance = self.getTolerance()
-      #logger("1", LOGGER_INFO)
-      
+
       if (self.expression == None):
         manager = ExpressionEvaluatorLocator.getManager()
         self.expression = manager.createExpression()
         self.expressionBuilder = manager.createExpressionBuilder()
         self.geomName = feature1.getType().getDefaultGeometryAttributeName()
-      
+
       line = feature1.getDefaultGeometry()
-      #lineTolerance = line.buffer(tolerance) #polygon
+
+      if line == None:
+        return
+
+      tolerance = self.getTolerance()
       numVertexLine = line.getNumVertices()
       lista = [line.getVertex(0), line.getVertex(numVertexLine-1)]
       
       for vertex in lista:
         noError = False
         print "One end of the line ", feature1.get("Name"), "is: ", vertex
-        print type(vertex)
-        #if( point==None ):
-         # return
-        #logger("1", LOGGER_INFO)
-        
+
+        if tolerance != 0:
+          vertexTolerance = vertex.buffer(tolerance)
+        else:
+          vertexTolerance = vertex
+
         theDataSet = self.getDataSet1()
-        #logger("2", LOGGER_INFO)
         if theDataSet.getSpatialIndex() != None:
-          #logger("if", LOGGER_INFO)
-          for reference in theDataSet.query(vertex):
+          for reference in theDataSet.query(vertexTolerance):
             #FeatureReference reference
-            # Misma feature
-            #logger("ref"+str(reference), LOGGER_INFO)
-            
-            
+
             feature = reference.getFeature()
             otherLine = feature.getDefaultGeometry()
-            #numVertexOtherLine = otherLine.getNumVertices()
-            
+
             print "Analyzing vertex of ", feature1.Name, " with line ", feature.Name
-            #if line.touches(otherLine):
-            
 
             if (reference.equals(feature1.getReference())):
               print "Same entity"
               print numVertexLine
-              #continue;
               if numVertexLine > 2:
                 endVertex = line.getVertex(0)
                 print type(endVertex)
                 cloneLine = line.cloneGeometry()
                 if lista.index(vertex) == 0:
                   cloneLine.removeVertex(0)
-                  if cloneLine.intersects(vertex):
+                  if cloneLine.intersects(vertexTolerance):
                     print "Intersects with itself"
                     noError = True
                     
@@ -108,20 +101,18 @@ class MustNotHaveDanglesLineRule(AbstractTopologyRule):
                     print "No intersects with itself"
                 else:
                   cloneLine.removeVertex(numVertexLine-1)
-                  if cloneLine.intersects(vertex):
+                  if cloneLine.intersects(vertexTolerance):
                     print "Intersects with itself"
                     noError = True
                     
                   else:
                     print "No intersects with itself"
-                    
-              
-            if (vertex.intersects(otherLine) and not line.equals(otherLine)):
+
+            if (vertexTolerance.intersects(otherLine) and not line.equals(otherLine)):
               print "Intersects vertex", feature1.Name, " with line ", feature.Name
               noError = True
               break
 
-                
           logger("previous report", LOGGER_INFO)
           if noError == False:
             error = vertex
@@ -175,7 +166,7 @@ class MustNotHaveDanglesLineRule(AbstractTopologyRule):
               "The point is dangling"
             )
         logger("end", LOGGER_INFO)
-    except: # Exception as ex:
+    except:
       #logger("2 Can't check feature."+str(ex), LOGGER_WARN)
       ex = sys.exc_info()[1]
       logger("Can't execute rule. Class Name:" + ex.__class__.__name__ + " Except:" + str(ex))
